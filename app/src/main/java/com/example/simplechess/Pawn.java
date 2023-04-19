61f507181ea2bed5959c1c6fee7bf5e2fa047e62
Dmitry Merinov <dymerinov@gmail.com>
2023-04-19 11:35:29 +0400

    comment code

--- app/src/main/java/com/example/simplechess/Pawn.java
+++ app/src/main/java/com/example/simplechess/Pawn.java
@@ -16,4 +16,11 @@ public class Pawn extends Figure {
     public boolean canMove(Position position) {
         return false;
     }
+//    @Override
+//    public void draw(Canvas canvas) {
+//        if (isDead) return;
+//        int CELL_SIZE = Math.min(canvas.getWidth(), canvas.getHeight()) / 8;
+//        canvas.drawBitmap(bitmap, position.x * CELL_SIZE, position.y * CELL_SIZE, null);
+//    }
+
 }
